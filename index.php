f528daadc01c65a5f561cc903ac7b205f2ee763f
Markus Pettersson <markus.pettersson@edu.huddinge.se>
2025-10-01 08:30:27 +0000

    Initial commit from PHPSandbox

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>PHP template</title>
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+
+<form action="login.php" method="post">
+    <h2>Logga in</h2>
+    <p>
+        <input type="text" placeholder="Användarnamn" name="user">
+    </p>
+    
+    <p>
+        <input type="password" placeholder="Lösenord" name="pass"><br>
+    </p>
+
+    <p>
+        <input type="submit" value="Logga in">
+    </p>
+</form>
+
+</body>
+</html>
